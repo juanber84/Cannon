c33097c278539ebf3b8723c3c07521a3fb8203ff
Juan <juan@iddeos.com>
2014-02-05 14:19:01 +0100

    license

--- cannon.php
+++ cannon.php
@@ -1,4 +1,25 @@
 <?php
+/*Copyright (c) 2014 Juan Berzal (www.juanberzal.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is furnished
+to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
 
 	$file = ".cannon/config.json";
 	
@@ -1089,7 +1110,7 @@ footer {color:#3d4748; font-size: 18px; font-family: Arial; text-align: center; 
                 </div>                
             </section>
             <footer>
-                <p>© Copyright 2014 Cannon Todos los derechos reservados.</p>
+                <p>© Copyright 2014 Cannon.</p>
             </footer>
         </div>
     </body>
